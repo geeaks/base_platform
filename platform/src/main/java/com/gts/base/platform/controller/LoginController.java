d48886c2a7c45d85a0c7d56c8bad1039581b4131
geeaks <vsflyhigh@qq.com>
2015-11-27 19:21:27 +0800

    day5 准备开工

--- platform/src/main/java/com/gts/base/platform/controller/LoginController.java
+++ platform/src/main/java/com/gts/base/platform/controller/LoginController.java
@@ -4,21 +4,75 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.common.collect.Maps;
 import com.gts.base.platform.utils.IdentifyingCode;
 import com.gts.base.platform.utils.enums.EnumSessionKey;
 
 @Controller
-public class LoginController {
+public class LoginController extends BaseController {
 	
+	public static Logger LOGGER = Logger.getLogger(LoginController.class);
+	
+	@RequestMapping("")
+	public String index(HttpServletRequest request, HttpServletResponse response, Model model){
+		return "index";
+	}
+	
+	@RequestMapping("/login")
+	@ResponseBody
+	public Map<String, Object> login(HttpServletRequest request, HttpServletResponse response, Model model){
+		Map<String, Object> map = new HashMap<String,Object>();
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		if(StringUtils.isBlank(userName) || StringUtils.isBlank(password)){
+			map.put("success", false);
+			map.put("msg", "登录名或密码输入错误");
+			return map;
+		}
+		map.put("success", true);
+		map.put("msg", "登录成功，登录名:【"+userName+"】 登录密码:【"+password+"】");
+		return map;
+	}
+	
+	/**
+	 * @Description: 登出
+	 * @param session
+	 * @return String 返回类型
+	 * @author gaoxiang
+	 * @date 2015年11月27日 下午7:19:05
+	 */
+	@RequestMapping("/logOut")
+	public String logOut(HttpSession session){
+		session.invalidate();
+		return "index";
+	}
+	
+	/**
+	 * @Description: 获取图片验证码
+	 * @param request
+	 * @param response
+	 * @throws IOException
+	 * @return void 返回类型
+	 * @author gaoxiang
+	 * @date 2015年11月27日 下午7:08:14
+	 */
 	@RequestMapping(value = "/getImgCode")
-	public void genImgCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	public void genImgCode(HttpSession session, HttpServletResponse response) throws IOException {
 		// 设置不缓存图片
 		response.setHeader("Pragma", "No-cache");
 		response.setHeader("Cache-Control", "No-cache");
@@ -40,27 +94,40 @@ public class LoginController {
 		g.setColor(idCode.getRandomColor(180, 200));
 		idCode.drawRandomLines(g, 160);
 		String idcodeStr = idCode.drawRandomString(4, g);
-		HttpSession session = request.getSession();
 		session.setAttribute(EnumSessionKey.IMG_CODE_KEY.getKey(), idcodeStr);
 		g.dispose();
 		ImageIO.write(image, "JPEG", response.getOutputStream());
 	}
 	
+	/**
+	 * @Description: 校验图片验证码
+	 * @param session
+	 * @param checkCode
+	 * @throws Exception
+	 * @return String 返回类型
+	 * @author gaoxiang
+	 * @date 2015年11月27日 下午7:08:25
+	 */
 	@RequestMapping(value = "/checkImgCode")
-	@ResponseBody
-	public String checkCode(HttpSession session, String checkCode) throws Exception {
+	public @ResponseBody Map<String,Object> checkCode(HttpSession session, String checkCode) throws Exception {
+		Map<String,Object> map = Maps.newHashMap();
 		String idCode = (String) session.getAttribute(EnumSessionKey.IMG_CODE_KEY.getKey());
-		if (checkCode != null && !checkCode.trim().equals("")) {
+		if (StringUtils.isNotBlank(checkCode)) {
 			if (idCode == null) {
-				return "验证码过期";
+				map.put("success", false);
+				map.put("msg","验证码过期");
 			} else if (!idCode.equalsIgnoreCase(checkCode.trim())) {
-				return "请输入正确的验证码";
+				map.put("success", false);
+				map.put("msg","请输入正确的验证码");
 			} else {
-				return "";
+				map.put("success", true);
+				map.put("msg","输入正确");
 			}
 		} else {
-			return "验证码不可为空";
+			map.put("success", true);
+			map.put("msg","验证码不可为空");
 		}
+		return map;
 	}
 	
 }
